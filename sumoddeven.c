9d6f3d30c692177292a74cb4f21e7bcecc478848
mekchandr <35653274+mekchandr@users.noreply.github.com>
2018-02-01 16:17:36 +0530

    Rename oddeven.c to sumoddeven.c

---
+++ sumoddeven.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int b;
+printf("Enter number",b);
+scanf("%d",&b);
+if(b/2==0)
+printf("The number is even");
+else
+printf("The number is odd");
+}
